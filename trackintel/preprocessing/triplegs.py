00ddca0e0afa4e05b3418eb84a9220c2290d556d
hamperj <hamperj@ethz.ch>
2020-01-16 18:01:21 +0100

    cleaned up

--- trackintel/preprocessing/triplegs.py
+++ trackintel/preprocessing/triplegs.py
@@ -9,8 +9,4 @@ def smoothen_triplegs(triplegs, method='douglas-peucker', epsilon = 1.0):
     input_copy = copy.deepcopy(triplegs)
     input_copy.geom = [LineString(ast.literal_eval(str(simplify_coords(input_copy.geom[i].coords, epsilon))))
                        for i in range(len(input_copy.geom))]
-    #coords = input_copy.geom[0].coords
-    #print(coords)
-    #print(simplify_coords(coords, 0.00000001))
-    print(type(input_copy.geom[0]))
     return input_copy
