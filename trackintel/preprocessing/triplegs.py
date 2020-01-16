addb53e5bd660129863c93570377b123c71f98aa
hamperj <hamperj@ethz.ch>
2020-01-16 18:05:54 +0100

    inserted comments

--- trackintel/preprocessing/triplegs.py
+++ trackintel/preprocessing/triplegs.py
@@ -4,7 +4,15 @@ import copy
 import ast
 
 def smoothen_triplegs(triplegs, method='douglas-peucker', epsilon = 1.0):
-    """Not implemented.
+    """reduces number of points while retaining structure of tripleg
+    Parameters
+    ----------
+    triplegs: shapely file
+        triplegs to be reduced
+    method: method used to smoothen
+        only one method available so far
+    epsilon: float
+        slack parameter, higher epsilon means removing more points
     """
     input_copy = copy.deepcopy(triplegs)
     input_copy.geom = [LineString(ast.literal_eval(str(simplify_coords(input_copy.geom[i].coords, epsilon))))
