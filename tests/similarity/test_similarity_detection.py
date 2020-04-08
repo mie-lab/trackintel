bd9bc569bef2b1a26586f08f05c6963f6cf8d93f
Sven Ruf <>
2020-04-08 20:23:41 +0200

    continued implement testing of similartiy module

--- tests/similarity/test_similarity_detection.py
+++ tests/similarity/test_similarity_detection.py
@@ -1,6 +1,26 @@
 # -*- coding: utf-8 -*-
 
 import trackintel as ti
+import numpy as np
+from ti.similarity.detection import similarity_detection
 
 class testSimilarityDetection:
-    def test_similarity_detection(self):
+    def test_similarity_detection_trsh(self):
+        
+        pfs = None #some test data
+        
+        sim = similarity_detection(pfs, 20)
+        assert sim is dict
+        assert all(value<=20 for value in sim.values())
+        
+    
+    def test_similarity_detection_no_trsh(self):
+        pfs = None #some test data
+        a = 1 # id of a specific tripleg
+        b = 2 # id of a specific trioleg
+        
+        sim = similarity_detection(pfs)
+        assert isinstance(sim, np.ndarray)
+        assert sim()
+    
+        
