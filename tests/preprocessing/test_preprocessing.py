2705ab844cd3a3e7ae6add5b7a2310bff29a6f8a
hongy <hongy_gz@foxmail.com>
2020-11-18 22:43:30 +0100

    add dataset level location extraction

--- tests/preprocessing/test_preprocessing.py
+++ tests/preprocessing/test_preprocessing.py
@@ -39,7 +39,23 @@ class TestPreprocessing():
         spts = pfs.as_positionfixes.extract_staypoints(method='sliding', dist_threshold=0, time_threshold=0)
         _, clusters = spts.as_staypoints.extract_locations(method='dbscan', epsilon=1e18, num_samples=1000)
         assert len(clusters) == 0, "With large hyperparameters, everything is an outlier"
-
-
-
-
+        
+    def test_cluster_staypoints_dbscan_user_dataset(self):
+        spts = ti.read_staypoints_csv('tests/data/geolife/geolife_staypoints.csv')
+        # take the first row and duplicate once
+        spts = spts.head(1)
+        spts = spts.append(spts, ignore_index=True)
+        # assign a different user_id to the second row
+        spts.iloc[1, 5] = 1
+        # duplicate for a certain number 
+        spts = spts.append([spts]*5,ignore_index=True)
+        _, locs_ds = spts.as_staypoints.extract_locations(method='dbscan', epsilon=10, 
+                                                          num_samples=0, distance_matrix_metric='haversine',
+                                                          agg_level='dataset')
+        _, locs_us = spts.as_staypoints.extract_locations(method='dbscan', epsilon=10, 
+                                                          num_samples=0, distance_matrix_metric='haversine',
+                                                          agg_level='user')
+        loc_ds_num = locs_ds['location_id'].unique().shape[0]
+        loc_us_num = locs_us['location_id'].unique().shape[0]
+        assert  loc_ds_num == 1, "Considering all staypointsat once, there will be only 1 location"
+        assert  loc_us_num == 2, "Considering user separately, there will be 2 location"
