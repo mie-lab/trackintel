fcc9759f22304cf1354d71f41f3e60b3b1a34b69
Yanan <7182055+zero31415@users.noreply.github.com>
2020-11-22 23:59:16 +0100

    changed the douglas peucker simplification function and the test function

--- tests/preprocessing/test_triplegs.py
+++ tests/preprocessing/test_triplegs.py
@@ -1,9 +1,10 @@
 import os
 
 import pandas as pd
-
+import trackintel as ti
 from trackintel.io.dataset_reader import read_geolife
 from trackintel.preprocessing.triplegs import generate_trips
+from trackintel.preprocessing.triplegs import smoothen_triplegs
 
 
 class TestGenerate_trips():
@@ -26,3 +27,24 @@ class TestGenerate_trips():
 
         spts, tpls, trips = generate_trips(spts, tpls, gap_threshold=15, id_offset=0)
         pd.testing.assert_frame_equal(trips_loaded, trips)
+
+    def test_douglas_peucker_algorithm_reduce_tripleg_length(self):
+        """
+        test the douglas peucker algorithm in simplifying triplegs
+        """
+        orig_file = 'tests/data/triplegs_with_too_many_points_test.csv'
+        tpls = ti.read_triplegs_csv(orig_file, sep=';')
+        tpls_smoothed = smoothen_triplegs(tpls, tolerance=0.0001)
+        line1 = tpls.iloc[0].geom
+        line1_smoothed = tpls_smoothed.iloc[0].geom
+        line2 = tpls.iloc[1].geom
+        line2_smoothed = tpls_smoothed.iloc[1].geom
+
+        print(line1)
+        print(line1_smoothed)
+        assert line1.length == line1_smoothed.length
+        assert line2.length == line2_smoothed.length
+        assert len(line1.coords) == 10
+        assert len(line2.coords) == 7
+        assert len(line1_smoothed.coords) == 4
+        assert len(line2_smoothed.coords) == 3
