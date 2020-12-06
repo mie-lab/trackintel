05abeb841aa7e748701ad7185e342caf8025b453
Henry Martin <martinhe@ethz.ch>
2020-12-06 23:42:22 +0100

    Postgis test (should fail)

--- tests/io/test_postgis_gpd.py
+++ tests/io/test_postgis_gpd.py
@@ -65,7 +65,7 @@ def connection_postgis():
     port = os.environ.get("PGPORT")
     try:
         con = psycopg2.connect(
-            dbname=dbname, user=user, password=password, host=host, port=port
+            dbname="test_geopandas2", user=user, password=password, host=host, port=port
         )
     except OperationalError:
         pytest.skip("Cannot connect with postgresql database")
