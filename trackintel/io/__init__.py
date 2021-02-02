89f3c4be09eae4acb62d4ecefb41fba395ac9356
Sven Ruf <>
2021-02-02 15:17:39 +0100

    Feat: support import from GeoDataFrames added trackintel/io/from_geopandas.py: Created functions that warp pd.rename to import GeoDataFrames into the trackintel model added tests for these functions in tests/io/test_io.py and datasets for these tests in tests/data (geojson files)

--- trackintel/io/__init__.py
+++ trackintel/io/__init__.py
@@ -2,23 +2,28 @@ from .file import read_positionfixes_csv
 from .file import write_positionfixes_csv
 from .postgis import read_positionfixes_postgis
 from .postgis import write_positionfixes_postgis
+from .from_geopandas import positionfixes_from_gpd
 
 from .file import read_triplegs_csv
 from .file import write_triplegs_csv
 from .postgis import read_triplegs_postgis
 from .postgis import write_triplegs_postgis
+from .from_geopandas import triplegs_from_gpd
 
 from .file import read_staypoints_csv
 from .file import write_staypoints_csv
 from .postgis import read_staypoints_postgis
 from .postgis import write_staypoints_postgis
+from .from_geopandas import staypoints_from_gpd
 
 from .file import read_locations_csv
 from .file import write_locations_csv
 from .postgis import read_locations_postgis
 from .postgis import write_locations_postgis
+from .from_geopandas import locations_from_gpd
 
 from .file import read_trips_csv
 from .file import write_trips_csv
 from .postgis import read_trips_postgis
 from .postgis import write_trips_postgis
+from .from_geopandas import trips_from_gpd
---
+++ trackintel/io/from_geopandas.py
@@ -0,0 +1,203 @@
+import geopandas as gpd
+import pandas as pd
+
+def positionfixes_from_gpd(gdf, tracked_at='tracked_at', user_id='user_id', geom='geom', mapper={}):
+    """
+    warps the pd.rename function to simplify the import of GeoDataFrames
+
+    Parameters
+    ----------
+    gdf : GeoDataFrame with valid point geometry, containing the positionfixes to import
+    tracked_at : str, name of the column storing the timestamps. The default is 'tracked_at'.
+    user_id : str, name of the column storing the user_id. The default is 'user_id'.
+    geom : str, name of the column storing the geometry. The default is 'geom'.
+    mapper : dict, further columns that should be renamed. 
+
+    Returns
+    -------
+    gdf : a validated positionfixes GeoDataFrame
+
+    """
+  
+    
+    columns = {tracked_at:'tracked_at', 
+               user_id:'user_id',
+               geom:'geom'}
+    columns.update(mapper)
+    
+    pfs = gdf.rename(columns=columns)
+    pfs = pfs.set_geometry('geom')
+    
+    assert pfs.as_positionfixes
+    
+
+    return pfs
+
+def staypoints_from_gpd(gdf, started_at='started_at', finished_at='finished_at', user_id='user_id', geom='geom', mapper={}):
+    """
+    warps the pd.rename function to simplify the import of GeoDataFrames
+
+    Parameters
+    ----------
+    gdf : GeoDataFrame with valid point geometry, containing the staypoints to import
+    started_at : str, name of the column storing the starttime of the staypoints. The default is 'started_at'.
+    finished_at : str, name of the column storing the endtime of the staypoints. The default is 'finished_at'.
+    user_id : str, name of the column storing the user_id. The default is 'user_id'.
+    geom : str, name of the column storing the geometry. The default is 'geom'.
+    mapper : dict, further columns that should be renamed. 
+
+    Returns
+    -------
+    gdf : a validated staypoints GeoDataFrame
+
+    """
+  
+    
+    columns = {started_at:'started_at',
+               finished_at:'finished_at',
+               user_id:'user_id',
+               geom:'geom'}
+    columns.update(mapper)
+    
+    stp = gdf.rename(columns=columns)
+    stp = stp.set_geometry('geom')
+    
+    assert stp.as_staypoints
+    
+
+    return stp
+
+def triplegs_from_gpd(gdf, started_at='started_at', finished_at='finished_at', user_id='user_id', geom='geometry', mapper={}):
+    """
+    warps the pd.rename function to simplify the import of GeoDataFrames
+
+    Parameters
+    ----------
+    gdf : GeoDataFrame with valid line geometry, containing the triplegs to import
+    started_at : str, name of the column storing the starttime of the triplegs. The default is 'started_at'.
+    finished_at : str, name of the column storing the endtime of the triplegs. The default is 'finished_at'.
+    user_id : str, name of the column storing the user_id. The default is 'user_id'.
+    geom : str, name of the column storing the geometry. The default is 'geom'.
+    mapper : dict, further columns that should be renamed. 
+
+    Returns
+    -------
+    gdf : a validated triplegs GeoDataFrame
+
+    """
+  
+    
+    columns = {started_at:'started_at',
+               finished_at:'finished_at',
+               user_id:'user_id',
+               geom:'geom'}
+    columns.update(mapper)
+    
+    tpl = gdf.rename(columns=columns)
+    tpl = tpl.set_geometry('geom')
+    
+    assert tpl.as_triplegs
+    
+
+    return tpl
+
+def trips_from_gpd(gdf, started_at='started_at', finished_at='finished_at', user_id='user_id', origin_staypoint_id='origin_staypoint_id', destination_staypoint_id='destination_staypoint_id', mapper={}):
+    """
+    warps the pd.rename function to simplify the import of GeoDataFrames
+
+    Parameters
+    ----------
+    gdf : GeoDataFrame with valid geometry, containing the trips to import
+    started_at : str, name of the column storing the starttime of the staypoints. The default is 'started_at'.
+    finished_at : str, name of the column storing the endtime of the staypoints. The default is 'finished_at'.
+    user_id : str, name of the column storing the user_id. The default is 'user_id'.
+    origin_staypoint_id : str, name of the column storing the staypoint_id of the start of the tripleg
+    destination_staypoint_id : str, name of the column storing the staypoint_id of the end of the tripleg
+    mapper : dict, further columns that should be renamed. 
+
+    Returns
+    -------
+    gdf : a validated trips GeoDataFrame
+
+    """
+  
+    
+    columns = {started_at:'started_at',
+               finished_at:'finished_at',
+               user_id:'user_id',
+               origin_staypoint_id:'origin_staypoint_id',
+               destination_staypoint_id:'destination_staypoint_id'
+               }
+    columns.update(mapper)
+    
+    tps = gdf.rename(columns=columns)
+    
+    assert tps.as_trips
+    
+
+    return tps
+
+def locations_from_gpd(gdf, user_id='user_id', center='center', mapper={}):
+    """
+    warps the pd.rename function to simplify the import of GeoDataFrames
+
+    Parameters
+    ----------
+    gdf : GeoDataFrame with valid point geometry, containing the locations to import
+    user_id : str, name of the column storing the user_id. The default is 'user_id'.
+    center : str, name of the column storing the geometry (Center of the location). The default is 'center'.
+    mapper : dict, further columns that should be renamed. 
+
+    Returns
+    -------
+    gdf : a validated locations GeoDataFrame
+
+    """
+  
+    
+    columns = {user_id:'user_id',
+               center:'center'}
+    columns.update(mapper)
+    
+    lcs = gdf.rename(columns=columns)
+    lcs = lcs.set_geometry('center')
+    
+    assert lcs.as_locations
+    
+
+    return lcs
+
+def tours_from_gpd(gdf, user_id='user_id',started_at='started_at', finished_at='finished_at', origin_destination_location_id='origin_destination_location_id', journey='journey', mapper={}):
+    """
+    warps the pd.rename function to simplify the import of GeoDataFrames
+
+    Parameters
+    ----------
+    gdf : GeoDataFrame with valid point geometry, containing the locations to import
+    user_id : str, name of the column storing the user_id. The default is 'user_id'.
+    started_at : str, name of the column storing the starttime of the staypoints. The default is 'started_at'.
+    finished_at : str, name of the column storing the endtime of the staypoints. The default is 'finished_at'.
+    origin_destination_location_id : the name of the column storing the id of the location where the tour starts and ends. The default is 'origin_destination_location_id'.
+    journey : str, name of the column storing the information (bool) if the tour is a journey. The default is 'journey'.
+    mapper : dict, further columns that should be renamed. 
+
+    Returns
+    -------
+    gdf : a validated locations GeoDataFrame
+
+    """
+  
+    
+    columns = {user_id:'user_id',
+               started_at:'tracked_at',
+               finished_at:'finished_at',
+               origin_destination_location_id:'origin_destination_location_id',
+               journey:'journey'}
+    columns.update(mapper)
+    
+    trs = gdf.rename(columns=columns)
+    
+    assert trs.as_tours
+    
+
+    return trs
