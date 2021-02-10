69846f8345e42328f47cb9b7afbe2b21f2c7d9a8
Sven Ruf <>
2021-02-10 11:07:00 +0100

    Added UserWarning for geographic and undefined coordinate systems Added test cases for different projections to check the UserWarnings

--- tests/analysis/test_transport_mode_identification.py
+++ tests/analysis/test_transport_mode_identification.py
@@ -25,8 +25,24 @@ class TestTransportModeIdentification:
     def test_check_correct_simple_coarse_identification(self):
         """Assert that the simple-coarse transport mode identification yields the correct results."""
         tpls = ti.read_triplegs_csv('tests/data/triplegs_transport_mode_identification.csv', sep=';')
-        tpls_transport_mode = tpls.as_triplegs.predict_transport_mode(method='simple-coarse')
+        with pytest.warns(UserWarning, match='Your data is not projected. WGS84 is assumed and for length calculation the haversine distance is used'):
+            tpls_transport_mode = tpls.as_triplegs.predict_transport_mode(method='simple-coarse')
+            
+        assert tpls_transport_mode.iloc[0]['mode'] == 'slow_mobility'
+        assert tpls_transport_mode.iloc[1]['mode'] == 'motorized_mobility'
+        assert tpls_transport_mode.iloc[2]['mode'] == 'fast_mobility'
+        
+        tpls = tpls.set_crs(epsg=4326)
+        with pytest.warns(UserWarning,match='Your data is in WGS84, for length calculation the haversine distance is used'):
+            tpls_transport_mode = tpls.as_triplegs.predict_transport_mode(method='simple-coarse')
+        
 
+        assert tpls_transport_mode.iloc[0]['mode'] == 'slow_mobility'
+        assert tpls_transport_mode.iloc[1]['mode'] == 'motorized_mobility'
+        assert tpls_transport_mode.iloc[2]['mode'] == 'fast_mobility'
+
+        tpls = tpls.to_crs(epsg=2056)
+        tpls_transport_mode = tpls.as_triplegs.predict_transport_mode(method='simple-coarse')
         assert tpls_transport_mode.iloc[0]['mode'] == 'slow_mobility'
         assert tpls_transport_mode.iloc[1]['mode'] == 'motorized_mobility'
         assert tpls_transport_mode.iloc[2]['mode'] == 'fast_mobility'
--- trackintel/analysis/transport_mode_identification.py
+++ trackintel/analysis/transport_mode_identification.py
@@ -1,7 +1,8 @@
-# -*- coding: utf-8 -*-
 
 import trackintel as ti
 from trackintel.geogr.distances import haversine_dist
+import geopandas as gpd
+import warnings
 
 
 def predict_transport_mode(triplegs, method='simple-coarse'):
@@ -35,12 +36,27 @@ def predict_transport_mode_simple_coarse(triplegs):
     :func:`trackintel.analysis.transport_mode_identification.predict_transport_mode`.
     """
     triplegs = triplegs.copy()
+    
+    wgs = False
+    if triplegs.crs == 4326:
+        wgs=True
+        warnings.warn('Your data is in WGS84, for length calculation the haversine distance is used')
+    elif triplegs.crs == None:
+        wgs=True
+        warnings.warn('Your data is not projected. WGS84 is assumed and for length calculation the haversine distance is used')
+    elif triplegs.crs.is_geographic:
+        warnings.warn('Your data is not in a projected coordinate system, length calculation will fail')
 
-    def identify_mode(tripleg):
+        
+
+    def identify_mode(tripleg,wgs):
         """Identifies the mode based on the (overall) tripleg speed."""
         # Computes distance over whole tripleg geometry (using the Haversine distance).
-        distance = sum([haversine_dist(pt1[0], pt1[1], pt2[0], pt2[1]) for pt1, pt2 \
-            in zip(tripleg.geom.coords[:-1], tripleg.geom.coords[1:])])
+        if wgs:
+            distance = sum([haversine_dist(pt1[0], pt1[1], pt2[0], pt2[1]) for pt1, pt2 \
+                in zip(tripleg.geom.coords[:-1], tripleg.geom.coords[1:])])
+        else:
+            distance = tripleg.geom.length
         duration = (tripleg['finished_at'] - tripleg['started_at']).total_seconds()
         speed = distance / duration
 
@@ -51,5 +67,5 @@ def predict_transport_mode_simple_coarse(triplegs):
         else:
             return 'fast_mobility'
 
-    triplegs['mode'] = triplegs.apply(identify_mode, axis=1)
+    triplegs['mode'] = triplegs.apply(lambda l: identify_mode(l,wgs), axis=1)
     return triplegs
