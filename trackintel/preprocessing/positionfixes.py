6428e7465aab66e7d1750331021aed56074e2933
Ye Hong <hongy_gz@foxmail.com>
2021-02-14 23:17:27 +0100

    change generate_staypoints sliding to groupby

--- trackintel/preprocessing/positionfixes.py
+++ trackintel/preprocessing/positionfixes.py
@@ -70,80 +70,24 @@ def generate_staypoints(positionfixes, method='sliding',
 
     if method == 'sliding':
         # Algorithm from Li et al. (2008). For details, please refer to the paper.
-        staypoint_id_counter = 0
-        ret_pfs['staypoint_id'] = -1  # this marks all that are not part of a SP
+        ret_pfs['staypoint_id'] = None  # this marks all that are not part of a SP
 
-        for user_id_this in ret_pfs['user_id'].unique():
-
-            positionfixes_user_this = ret_pfs.loc[ret_pfs['user_id'] == user_id_this]  # this is no copy
-
-            pfs = positionfixes_user_this.sort_values('tracked_at').to_dict('records')
-            num_pfs = len(pfs)
-
-            posfix_staypoint_matching = {}
-
-            i = 0
-            j = 0  # is zero because it gets incremented in the beginning
-            while i < num_pfs:
-                if j == num_pfs:
-                    # We're at the end, this can happen if in the last "bin", 
-                    # the dist_threshold is never crossed anymore.
-                    break
-                else:
-                    j = i + 1
-                while j < num_pfs:
-                    # TODO: Can we make distance function independent of projection?
-                    dist = dist_func(pfs[i][name_geocol].x, pfs[i][name_geocol].y,
-                                     pfs[j][name_geocol].x, pfs[j][name_geocol].y)
-
-                    if dist > dist_threshold:
-                        delta_t = pfs[j]['tracked_at'] - pfs[i]['tracked_at']
-                        if delta_t.total_seconds() > time_threshold:
-                            staypoint = {}
-                            staypoint['user_id'] = pfs[i]['user_id']
-                            staypoint[name_geocol] = Point(np.mean([pfs[k][name_geocol].x for k in range(i, j)]),
-                                                           np.mean([pfs[k][name_geocol].y for k in range(i, j)]))
-                            if elevation_flag:
-                                staypoint['elevation'] = np.mean([pfs[k]['elevation'] for k in range(i, j)])
-                            staypoint['started_at'] = pfs[i]['tracked_at']
-                            staypoint['finished_at'] = pfs[j - 1][
-                                'tracked_at']  # TODO: should this not be j-1? because j is not part of the staypoint. DB: Changed.
-                            staypoint['id'] = staypoint_id_counter
-
-                            # store matching 
-                            posfix_staypoint_matching[staypoint_id_counter] = [pfs[k]['id'] for k in range(i, j)]
-                            staypoint_id_counter += 1
-
-                            # add staypoint
-                            ret_spts = ret_spts.append(staypoint, ignore_index=True)
-
-                            # TODO Discussion: Is this last point really a staypoint? As we don't know if the
-                            #      person "moves on" afterwards...
-                            if j == num_pfs - 1:
-                                staypoint = {}
-                                staypoint['user_id'] = pfs[j]['user_id']
-                                staypoint[name_geocol] = Point(pfs[j][name_geocol].x, pfs[j][name_geocol].y)
-                                if elevation_flag:
-                                    staypoint['elevation'] = pfs[j]['elevation']
-                                staypoint['started_at'] = pfs[j]['tracked_at']
-                                staypoint['finished_at'] = pfs[j]['tracked_at']
-                                staypoint['id'] = staypoint_id_counter
-
-                                # store matching
-                                posfix_staypoint_matching[staypoint_id_counter] = [
-                                    pfs[j]['id']]  # rather [k for k in range(i, j)]?
-                                staypoint_id_counter += 1
-                                ret_spts = ret_spts.append(staypoint, ignore_index=True)
-                        i = j
-                        break
-                    j = j + 1
-
-            # add matching to original positionfixes (for every user)
-
-            for staypoints_id, posfix_idlist in posfix_staypoint_matching.items():
-                # note that we use .loc because above we have saved the id 
-                # of the positionfixes not thier absolut position
-                ret_pfs.loc[posfix_idlist, 'staypoint_id'] = staypoints_id
+        ret_spts = ret_pfs.groupby('user_id', as_index=False).apply(_generate_staypoints_sliding_user, 
+                                                                    name_geocol, 
+                                                                    elevation_flag,
+                                                                    dist_threshold, 
+                                                                    time_threshold, 
+                                                                    dist_func=haversine_dist).reset_index(drop=True)
+        ret_spts['id'] = np.arange(len(ret_spts))
+        
+        stps2pfs_map = ret_spts[['id', 'pfs_id']].set_index('id').to_dict()['pfs_id']
+        
+        ls = []
+        for key, values in stps2pfs_map.items():
+            for value in values:
+                ls.append([value, key])
+        temp = pd.DataFrame(ls, columns=['id', 'staypoint_id']).set_index('id')
+        ret_pfs = ret_pfs.join(temp, how='left')
 
 
     elif method == 'dbscan':
@@ -386,3 +330,73 @@ def generate_triplegs(positionfixes, staypoints=None, *args, **kwargs):
     # todo: triplegs dataframe has use the index as id
     # todo: proposed fix: ret_triplegs = ret_triplegs.set_index('id')
     return ret_pfs, ret_tpls
+
+
+def _generate_staypoints_sliding_user(df, 
+                                      name_geocol, 
+                                      elevation_flag,
+                                      dist_threshold=50, 
+                                      time_threshold= 300, 
+                                      dist_func=haversine_dist):
+    ret_spts = pd.DataFrame(columns=['id', 'user_id', 'started_at', 'finished_at', 'geom'])
+    
+    pfs = df.sort_values('tracked_at').to_dict('records')
+    num_pfs = len(pfs)
+
+
+    i = 0
+    j = 0  # is zero because it gets incremented in the beginning
+    while i < num_pfs:
+        if j == num_pfs:
+            # We're at the end, this can happen if in the last "bin", 
+            # the dist_threshold is never crossed anymore.
+            break
+        else:
+            j = i + 1
+        while j < num_pfs:
+            # TODO: Can we make distance function independent of projection?
+            dist = dist_func(pfs[i][name_geocol].x, pfs[i][name_geocol].y,
+                                pfs[j][name_geocol].x, pfs[j][name_geocol].y)
+
+            if dist > dist_threshold:
+                delta_t = pfs[j]['tracked_at'] - pfs[i]['tracked_at']
+                if delta_t.total_seconds() > time_threshold:
+                    staypoint = {}
+                    staypoint['user_id'] = pfs[i]['user_id']
+                    staypoint[name_geocol] = Point(np.mean([pfs[k][name_geocol].x for k in range(i, j)]),
+                                                    np.mean([pfs[k][name_geocol].y for k in range(i, j)]))
+                    if elevation_flag:
+                        staypoint['elevation'] = np.mean([pfs[k]['elevation'] for k in range(i, j)])
+                    staypoint['started_at'] = pfs[i]['tracked_at']
+                    staypoint['finished_at'] = pfs[j - 1][
+                        'tracked_at']  # TODO: should this not be j-1? because j is not part of the staypoint. DB: Changed.
+                    # staypoint['id'] = staypoint_id_counter
+
+                    # store matching 
+                    staypoint['pfs_id'] = [pfs[k]['id'] for k in range(i, j)]
+                    # posfix_staypoint_matching[staypoint_id_counter] = [pfs[k]['id'] for k in range(i, j)]
+                    # staypoint_id_counter += 1
+
+                    # add staypoint
+                    ret_spts = ret_spts.append(staypoint, ignore_index=True)
+
+                    # TODO Discussion: Is this last point really a staypoint? As we don't know if the
+                    #      person "moves on" afterwards...
+                    if j == num_pfs - 1:
+                        staypoint = {}
+                        staypoint['user_id'] = pfs[j]['user_id']
+                        staypoint[name_geocol] = Point(pfs[j][name_geocol].x, pfs[j][name_geocol].y)
+                        if elevation_flag:
+                            staypoint['elevation'] = pfs[j]['elevation']
+                        staypoint['started_at'] = pfs[j]['tracked_at']
+                        staypoint['finished_at'] = pfs[j]['tracked_at']
+                        # staypoint['id'] = staypoint_id_counter
+                        
+                        staypoint['pfs_id'] = [pfs[j]['id']]
+
+                        ret_spts = ret_spts.append(staypoint, ignore_index=True)
+                i = j
+                break
+            j = j + 1
+    
+    return ret_spts
