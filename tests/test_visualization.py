a7d50ca112541a8781b74ed775e9b4265c1aa4c8
Christof Leutenegger <christof.leutenegger@hotmail.com>
2021-03-02 17:58:09 +0100

    ENH: Added test for plotting locations.

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -29,3 +29,13 @@ class TestIO:
         stps.as_staypoints.plot(out_filename=tmp_file, radius=0.01, positionfixes=pfs, plot_osm=False)
         assert os.path.exists(tmp_file)
         os.remove(tmp_file)
+
+    def test_locations_plot(self):
+        tmp_file = 'tests/data/locations_plot.png'
+        pfs = pfs = ti.read_positionfixes_csv('tests/data/positionfixes.csv', sep=';')
+        stps = ti.read_staypoints_csv('tests/data/staypoints.csv', sep=';')
+        locs = ti.read_locations_csv('tests/data/locations.csv', sep=';')
+        locs.as_locations.plot(out_filename=tmp_file, radius=120, positionfixes=pfs,
+                               staypoints=stps, staypoints_radius=100, plot_osm=False)
+        assert os.path.exists(tmp_file)
+        os.remove(tmp_file)
