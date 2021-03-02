87b201e5cbeb5e8dcb309f63bc43d5716e48410d
Christof Leutenegger <christof.leutenegger@hotmail.com>
2021-03-02 17:13:17 +0100

    CLN: changed parameters according to changes in visualization

--- examples/preprocess_trajectories.py
+++ examples/preprocess_trajectories.py
@@ -7,10 +7,8 @@ sys.path.append("../trackintel")
 import logging
 
 import matplotlib.pyplot as plt
+
 import trackintel as ti
-
-from trackintel.geogr.distances import meters_to_decimal_degrees
-
 
 logging.basicConfig(filename='examples/log/preprocessing.log', level=logging.INFO, filemode='w')
 
@@ -18,19 +16,18 @@ logging.basicConfig(filename='examples/log/preprocessing.log', level=logging.INF
 pfs = ti.read_positionfixes_csv('examples/data/geolife_trajectory.csv', sep=';')
 pfs.as_positionfixes.plot(out_filename='examples/out/gpsies_trajectory_positionfixes.png', plot_osm=True)
 
-spts = pfs.as_positionfixes.extract_staypoints(method='sliding', dist_threshold=100, time_threshold=5*60)
+spts = pfs.as_positionfixes.generate_staypoints(method='sliding', dist_threshold=100, time_threshold=5*60)[1]
 spts.as_staypoints.plot(out_filename='examples/out/gpsies_trajectory_staypoints.png',
-                        radius=meters_to_decimal_degrees(100, 47.5), positionfixes=pfs, plot_osm=True)
+                        radius=100, positionfixes=pfs, plot_osm=True)
 
-plcs = spts.as_staypoints.extract_locations(method='dbscan', epsilon=meters_to_decimal_degrees(120, 47.5), 
-                                         num_samples=3)
-plcs.as_locations.plot(out_filename='examples/out/gpsies_trajectory_locations.png', radius=meters_to_decimal_degrees(120, 47.5), 
-                    positionfixes=pfs, staypoints=spts, staypoints_radius=meters_to_decimal_degrees(100, 47.5), 
-                    plot_osm=True)
+plcs = spts.as_staypoints.generate_locations(method='dbscan', epsilon=0.01, num_samples=3)[1]
+plcs.as_locations.plot(out_filename='examples/out/gpsies_trajectory_locations.png', radius=120,
+                       positionfixes=pfs, staypoints=spts, staypoints_radius=100,
+                       plot_osm=True)
 
-tpls = pfs.as_positionfixes.extract_triplegs()
-tpls.as_triplegs.plot(out_filename='examples/out/gpsies_trajectory_triplegs.png', staypoints=spts, 
-                      staypoints_radius=meters_to_decimal_degrees(100, 47.5))
+tpls = pfs.as_positionfixes.generate_triplegs(staypoints=spts)[1]
+tpls.as_triplegs.plot(out_filename='examples/out/gpsies_trajectory_triplegs.png', staypoints=spts,
+                      staypoints_radius=100, plot_osm=True)
 
 sys.exit(0)
 
@@ -38,18 +35,18 @@ sys.exit(0)
 pfs = ti.read_positionfixes_csv('data/geolife_trajectory.csv', sep=';')
 pfs.as_positionfixes.plot(out_filename='out/geolife_trajectory_positionfixes.png', plot_osm=False)
 
-spts = pfs.as_positionfixes.extract_staypoints(method='sliding', dist_threshold=100, time_threshold=10*60)
+spts = pfs.as_positionfixes.generate_staypoints(method='sliding', dist_threshold=100, time_threshold=10*60)[1]
 spts.as_staypoints.plot(out_filename='out/geolife_trajectory_staypoints.png',
-                        radius=meters_to_decimal_degrees(100, 47.5), positionfixes=pfs, plot_osm=True)
+                        radius=100, positionfixes=pfs, plot_osm=True)
 
 # Google trajectory.
 positionfixes = ti.read_positionfixes_csv('data/google_trajectory.csv', sep=';')
-spts = pfs.as_positionfixes.extract_staypoints(method='sliding', dist_threshold=75, time_threshold=10*60)
+spts = pfs.as_positionfixes.generate_staypoints(method='sliding', dist_threshold=75, time_threshold=10*60)[1]
 spts.as_staypoints.plot(out_filename='out/google_trajectory_staypoints.png',
-                        radius=meters_to_decimal_degrees(75, 47.5), positionfixes=pfs, plot_osm=True)
+                        radius=75, positionfixes=pfs, plot_osm=True)
 
 # Posmo trajectory.
 positionfixes = ti.read_positionfixes_csv('data/posmo_trajectory.csv', sep=';')
-spts = pfs.as_positionfixes.extract_staypoints(method='sliding', dist_threshold=50, time_threshold=1*60)
+spts = pfs.as_positionfixes.generate_staypoints(method='sliding', dist_threshold=50, time_threshold=1*60)[1]
 spts.as_staypoints.plot(out_filename='out/posmo_trajectory_staypoints.png',
-                        radius=meters_to_decimal_degrees(50, 47.5), positionfixes=pfs, plot_osm=False)
+                        radius=50, positionfixes=pfs, plot_osm=False)
