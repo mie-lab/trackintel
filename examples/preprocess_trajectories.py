3b180f629a6707bbe3a62e562cbb81894f52afe3
Christof Leutenegger <christof.leutenegger@hotmail.com>
2021-03-08 21:32:13 +0100

    CLN: Used matlab style reference for unused function returns.

--- examples/preprocess_trajectories.py
+++ examples/preprocess_trajectories.py
@@ -16,16 +16,16 @@ logging.basicConfig(filename='examples/log/preprocessing.log', level=logging.INF
 pfs = ti.read_positionfixes_csv('examples/data/geolife_trajectory.csv', sep=';')
 pfs.as_positionfixes.plot(out_filename='examples/out/gpsies_trajectory_positionfixes.png', plot_osm=True)
 
-spts = pfs.as_positionfixes.generate_staypoints(method='sliding', dist_threshold=100, time_threshold=5*60)[1]
+_, spts = pfs.as_positionfixes.generate_staypoints(method='sliding', dist_threshold=100, time_threshold=5*60)
 spts.as_staypoints.plot(out_filename='examples/out/gpsies_trajectory_staypoints.png',
                         radius=100, positionfixes=pfs, plot_osm=True)
 
-plcs = spts.as_staypoints.generate_locations(method='dbscan', epsilon=0.01, num_samples=3)[1]
+_, plcs = spts.as_staypoints.generate_locations(method='dbscan', epsilon=0.01, num_samples=3)
 plcs.as_locations.plot(out_filename='examples/out/gpsies_trajectory_locations.png', radius=120,
                        positionfixes=pfs, staypoints=spts, staypoints_radius=100,
                        plot_osm=True)
 
-tpls = pfs.as_positionfixes.generate_triplegs(staypoints=spts)[1]
+_, tpls = pfs.as_positionfixes.generate_triplegs(staypoints=spts)
 tpls.as_triplegs.plot(out_filename='examples/out/gpsies_trajectory_triplegs.png', staypoints=spts,
                       staypoints_radius=100, plot_osm=True)
 
@@ -35,18 +35,18 @@ tpls.as_triplegs.plot(out_filename='examples/out/gpsies_trajectory_triplegs.png'
 pfs = ti.read_positionfixes_csv('examples/data/geolife_trajectory.csv', sep=';')
 pfs.as_positionfixes.plot(out_filename='examples/out/geolife_trajectory_positionfixes.png', plot_osm=False)
 
-spts = pfs.as_positionfixes.generate_staypoints(method='sliding', dist_threshold=100, time_threshold=10*60)[1]
+_, spts = pfs.as_positionfixes.generate_staypoints(method='sliding', dist_threshold=100, time_threshold=10*60)
 spts.as_staypoints.plot(out_filename='examples/out/geolife_trajectory_staypoints.png',
                         radius=100, positionfixes=pfs, plot_osm=True)
 
 # Google trajectory.
 positionfixes = ti.read_positionfixes_csv('examples/data/google_trajectory.csv', sep=';')
-spts = pfs.as_positionfixes.generate_staypoints(method='sliding', dist_threshold=75, time_threshold=10*60)[1]
+_, spts = pfs.as_positionfixes.generate_staypoints(method='sliding', dist_threshold=75, time_threshold=10*60)
 spts.as_staypoints.plot(out_filename='examples/out/google_trajectory_staypoints.png',
                         radius=75, positionfixes=pfs, plot_osm=True)
 
 # Posmo trajectory.
 positionfixes = ti.read_positionfixes_csv('examples/data/posmo_trajectory.csv', sep=';')
-spts = pfs.as_positionfixes.generate_staypoints(method='sliding', dist_threshold=50, time_threshold=1*60)[1]
+_, spts = pfs.as_positionfixes.generate_staypoints(method='sliding', dist_threshold=50, time_threshold=1*60)
 spts.as_staypoints.plot(out_filename='examples/out/posmo_trajectory_staypoints.png',
                         radius=50, positionfixes=pfs, plot_osm=False)
