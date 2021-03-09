c3afb43bce01161dc4bf05ac58f5cff6d6085746
Henry Martin <martinhe@ethz.ch>
2021-03-09 09:23:23 +0100

    BUG: Fixed several errors in preprocess_trajectories.py -  Google and Posmo trajectories used wrong staypoint variable -  There were two versions of the posmo trajecotires: posmo_trajectory.csv and posmo_trajectory_2. posmo_trajectory.csv had corrupted (repeated very often) timestamps. I deleted posmo_trajectory.csv and renamed posmo_trajectory_2.csv.

--- examples/preprocess_trajectories.py
+++ examples/preprocess_trajectories.py
@@ -12,16 +12,16 @@ import trackintel as ti
 
 logging.basicConfig(filename='examples/log/preprocessing.log', level=logging.INFO, filemode='w')
 
-# GPSies trajectory.
-pfs = ti.read_positionfixes_csv('examples/data/geolife_trajectory.csv', sep=';')
-pfs.as_positionfixes.plot(out_filename='examples/out/gpsies_trajectory_positionfixes.png', plot_osm=True)
+# GPS trajectory.
+pfs = ti.read_positionfixes_csv('examples/data/geolife_trajectory.csv', sep=';', crs='EPSG:4326', index_col=None)
+pfs.as_positionfixes.plot(out_filename='examples/out/gps_trajectory_positionfixes.png', plot_osm=True)
 
 _, spts = pfs.as_positionfixes.generate_staypoints(method='sliding', dist_threshold=100, time_threshold=5*60)
-spts.as_staypoints.plot(out_filename='examples/out/gpsies_trajectory_staypoints.png',
+spts.as_staypoints.plot(out_filename='examples/out/gps_trajectory_staypoints.png',
                         radius=100, positionfixes=pfs, plot_osm=True)
 
-_, plcs = spts.as_staypoints.generate_locations(method='dbscan', epsilon=0.01, num_samples=3)
-plcs.as_locations.plot(out_filename='examples/out/gpsies_trajectory_locations.png', radius=120,
+_, locs = spts.as_staypoints.generate_locations(method='dbscan', epsilon=0.01, num_samples=3)
+locs.as_locations.plot(out_filename='examples/out/gps_trajectory_locations.png', radius=120,
                        positionfixes=pfs, staypoints=spts, staypoints_radius=100,
                        plot_osm=True)
 
@@ -29,10 +29,8 @@ _, tpls = pfs.as_positionfixes.generate_triplegs(staypoints=spts)
 tpls.as_triplegs.plot(out_filename='examples/out/gpsies_trajectory_triplegs.png', staypoints=spts,
                       staypoints_radius=100, plot_osm=True)
 
-# sys.exit(0)
-
 # Geolife trajectory.
-pfs = ti.read_positionfixes_csv('examples/data/geolife_trajectory.csv', sep=';')
+pfs = ti.read_positionfixes_csv('examples/data/geolife_trajectory.csv', sep=';', crs='EPSG:4326', index_col=None)
 pfs.as_positionfixes.plot(out_filename='examples/out/geolife_trajectory_positionfixes.png', plot_osm=False)
 
 _, spts = pfs.as_positionfixes.generate_staypoints(method='sliding', dist_threshold=100, time_threshold=10*60)
@@ -40,13 +38,14 @@ spts.as_staypoints.plot(out_filename='examples/out/geolife_trajectory_staypoints
                         radius=100, positionfixes=pfs, plot_osm=True)
 
 # Google trajectory.
-positionfixes = ti.read_positionfixes_csv('examples/data/google_trajectory.csv', sep=';')
+pfs = ti.read_positionfixes_csv('examples/data/google_trajectory.csv', sep=';', crs='EPSG:4326', index_col=None)
 _, spts = pfs.as_positionfixes.generate_staypoints(method='sliding', dist_threshold=75, time_threshold=10*60)
 spts.as_staypoints.plot(out_filename='examples/out/google_trajectory_staypoints.png',
                         radius=75, positionfixes=pfs, plot_osm=True)
 
 # Posmo trajectory.
-positionfixes = ti.read_positionfixes_csv('examples/data/posmo_trajectory.csv', sep=';')
+pfs = ti.read_positionfixes_csv('examples/data/posmo_trajectory.csv', sep=';', crs='EPSG:4326', index_col=None,
+                                tz='UTC')
 _, spts = pfs.as_positionfixes.generate_staypoints(method='sliding', dist_threshold=50, time_threshold=1*60)
 spts.as_staypoints.plot(out_filename='examples/out/posmo_trajectory_staypoints.png',
                         radius=50, positionfixes=pfs, plot_osm=False)
