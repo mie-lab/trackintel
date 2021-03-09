61b90fc4fb49783bc6201c5a96e3094168c57ff6
Christof Leutenegger <49416778+bifbof@users.noreply.github.com>
2021-03-09 10:36:04 +0100

    ENH: Updated plotting functions to be more versatile (#135)
    * Update positionfixes visualization for a more object oriented design.
    
    * Return axis of positionfixes plotting.
    
    * Radius to meter, calling positionfixes for their plotting
    
    * bugfixes
    
    * added case for plotting osm or not
    
    * added crs check and cascading plotting
    
    * Added CRS checking for triplegs and cascading plotting.
    
    * DOC: added information for radius
    
    * CLN: changed parameters according to changes in visualization
    
    * DOC: correction of example
    
    * CLN: fixed input and output paths
    
    * ENH: Added test for plotting locations.
    
    * CLN: Used matlab style reference for unused function returns.
    
    * DOC: Added example for plotting of triplegs.
    
    * TST: Added test for crs warning.
    
    * BUG: Replaced "Warning" with "warnings.warn"
    
    * BUG: Fixed several errors in preprocess_trajectories.py
    -  Google and Posmo trajectories used wrong staypoint variable
    -  There were two versions of the posmo trajecotires: posmo_trajectory.csv and posmo_trajectory_2. posmo_trajectory.csv had corrupted (repeated very often) timestamps. I deleted posmo_trajectory.csv and renamed posmo_trajectory_2.csv.
    
    * CLN: Docstrings for tests and PEP8
    
    Co-authored-by: Henry Martin <martinhe@ethz.ch>

--- trackintel/visualization/staypoints.py
+++ trackintel/visualization/staypoints.py
@@ -1,14 +1,16 @@
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
+import warnings
 
+from trackintel.geogr.distances import meters_to_decimal_degrees
 from trackintel.visualization.osm import plot_osm_streets
 from trackintel.visualization.util import regular_figure, save_fig
 
 
-def plot_staypoints(staypoints, out_filename=None, radius=None, positionfixes=None, plot_osm=False):
+def plot_staypoints(staypoints, out_filename=None, radius=None, positionfixes=None, plot_osm=False, axis=None):
     """Plots staypoints (optionally to a file). You can specify the radius with which 
     each staypoint should be drawn, as well as if underlying positionfixes and OSM streets
-    should be drawn.
+    should be drawn. The data gets transformed to wgs84 for the plotting.
 
     Parameters
     ----------
@@ -19,7 +21,7 @@ def plot_staypoints(staypoints, out_filename=None, radius=None, positionfixes=No
         The file to plot to, if this is not set, the plot will simply be shown.
 
     radius : float, optional
-        The radius with which circles around staypoints should be drawn.
+        The radius in meter with which circles around staypoints should be drawn.
 
     positionfixes : GeoDataFrame, optional
         If available, some positionfixes that can additionally be plotted.
@@ -28,41 +30,48 @@ def plot_staypoints(staypoints, out_filename=None, radius=None, positionfixes=No
         If this is set to True, it will download an OSM street network and plot 
         below the staypoints.
 
+    axis : matplotlib.pyplot.Artist, optional
+        axis on which to draw the plot
+
     Examples
     --------
-    >>> df.as_staypoints.plot('output.png', radius=10, positionfixes=pdf, plot_osm=True)
+    >>> df.as_staypoints.plot('output.png', radius=100, positionfixes=pdf, plot_osm=True)
     """
-    _, ax = regular_figure()
+    if axis is None:
+        _, ax = regular_figure()
+    else:
+        ax = axis
     name_geocol = staypoints.geometry.name
 
+    crs_wgs84 = 'EPSG:4326'
+    if staypoints.crs is None:
+        warnings.warn("Coordinate System (CRS) is not set, default to WGS84.")
+        staypoints.crs = crs_wgs84
+    elif staypoints.crs != crs_wgs84:
+        staypoints = staypoints.to_crs(crs_wgs84)
+
     if positionfixes is not None:
-        west = positionfixes.geometry.x.min() - 0.01
-        east = positionfixes.geometry.x.max() + 0.01
-        north = positionfixes.geometry.y.max() + 0.01
-        south = positionfixes.geometry.y.min() - 0.01
+        positionfixes.as_positionfixes.plot(plot_osm=plot_osm, axis=ax)
     else:
         west = staypoints.geometry.x.min() - 0.03
         east = staypoints.geometry.x.max() + 0.03
         north = staypoints.geometry.y.max() + 0.03
         south = staypoints.geometry.y.min() - 0.03
-
-    if plot_osm:
-        plot_osm_streets(north, south, east, west, ax)
-
-    if positionfixes is not None:
-        positionfixes.plot(ax=ax, markersize=0.5, zorder=2)
+        if plot_osm:
+            plot_osm_streets(north, south, east, west, ax)
+        ax.set_xlim([west, east])
+        ax.set_ylim([south, north])
 
     if radius is None:
-        radius = 5
+        radius = 100
+    center_latitude = (ax.get_ylim()[0] + ax.get_ylim()[1]) / 2
+    radius = meters_to_decimal_degrees(radius, center_latitude)
     for pt in staypoints.to_dict('records'):
         circle = mpatches.Circle((pt[name_geocol].x, pt[name_geocol].y), radius,
                                  facecolor='none', edgecolor='g', zorder=3)
         ax.add_artist(circle)
 
-    ax.set_xlim([west, east])
-    ax.set_ylim([south, north])
-
     if out_filename is not None:
         save_fig(out_filename, formats=['png'])
-    else:
+    elif axis is None:
         plt.show()
