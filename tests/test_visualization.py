137ab729ae3e0f15ac3b41ce46124ceea1f09108
Christof Leutenegger <christof.leutenegger@hotmail.com>
2021-03-09 18:19:18 +0100

    TST: Fixed unexpected warnings.

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -13,7 +13,7 @@ class TestIO:
 
         tmp_file = os.path.join('tests', 'data', 'positionfixes_plot.png')
         pfs_file = os.path.join('tests', 'data', 'positionfixes.csv')
-        pfs = ti.read_positionfixes_csv(pfs_file, sep=';', index_col='id')
+        pfs = ti.read_positionfixes_csv(pfs_file, sep=';', index_col='id', crs='EPSG:4326')
         pfs.as_positionfixes.plot(out_filename=tmp_file, plot_osm=False)
         assert os.path.exists(tmp_file)
         os.remove(tmp_file)
@@ -23,10 +23,10 @@ class TestIO:
 
         tmp_file = os.path.join('tests', 'data', 'triplegs_plot.png')
         pfs_file = os.path.join('tests', 'data', 'positionfixes.csv')
-        pfs = ti.read_positionfixes_csv(pfs_file, sep=';', index_col='id')
+        pfs = ti.read_positionfixes_csv(pfs_file, sep=';', index_col='id', crs='EPSG:4326')
 
         tpls_file = os.path.join('tests', 'data', 'triplegs.csv')
-        tpls = ti.read_triplegs_csv(tpls_file, sep=';', index_col='id')
+        tpls = ti.read_triplegs_csv(tpls_file, sep=';', index_col='id', crs='EPSG:4326')
         tpls.as_triplegs.plot(out_filename=tmp_file, positionfixes=pfs, plot_osm=False)
         assert os.path.exists(tmp_file)
         os.remove(tmp_file)
@@ -36,10 +36,10 @@ class TestIO:
 
         tmp_file = os.path.join('tests', 'data', 'staypoints_plot.png')
         pfs_file = os.path.join('tests', 'data', 'positionfixes.csv')
-        pfs = ti.read_positionfixes_csv(pfs_file, sep=';', index_col='id')
+        pfs = ti.read_positionfixes_csv(pfs_file, sep=';', index_col='id', crs='EPSG:4326')
 
         stps_file = os.path.join('tests', 'data', 'staypoints.csv')
-        stps = ti.read_staypoints_csv(stps_file, sep=';', index_col='id')
+        stps = ti.read_staypoints_csv(stps_file, sep=';', index_col='id', crs='EPSG:4326')
         stps.as_staypoints.plot(out_filename=tmp_file, radius=0.01, positionfixes=pfs, plot_osm=False)
         assert os.path.exists(tmp_file)
         os.remove(tmp_file)
@@ -48,9 +48,12 @@ class TestIO:
         """Use trackintel visualization function to plot locations and check if file exists."""
 
         tmp_file = os.path.join("tests", "data", "locations_plot.png")
-        pfs = pfs = ti.read_positionfixes_csv('tests/data/positionfixes.csv', sep=';')
-        stps = ti.read_staypoints_csv('tests/data/staypoints.csv', sep=';')
-        locs = ti.read_locations_csv('tests/data/locations.csv', sep=';')
+        pfs_file = os.path.join('tests', 'data', 'positionfixes.csv')
+        pfs = pfs = ti.read_positionfixes_csv(pfs_file, sep=';', index_col='id', crs='EPSG:4326')
+        stps_file = os.path.join('tests', 'data', 'staypoints.csv')
+        stps = ti.read_staypoints_csv(stps_file, sep=';', index_col='id', crs='EPSG:4326')
+        locs_file = os.path.join('tests', 'data', 'locations.csv')
+        locs = ti.read_locations_csv(locs_file, sep=';', index_col='id', crs='EPSG:4326')
         locs.as_locations.plot(out_filename=tmp_file, radius=120, positionfixes=pfs,
                                staypoints=stps, staypoints_radius=100, plot_osm=False)
         assert os.path.exists(tmp_file)
