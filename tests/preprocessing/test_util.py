7a393c37bf24f0265e5bcbed283e886e3a1bb0fe
Henry Martin <martinhe@ethz.ch>
2021-03-13 02:24:42 +0100

    ENH: A function that calculates the overlap of two intervals This is a useful tool to match triplegs from different sources

---
+++ tests/preprocessing/test_util.py
@@ -0,0 +1,33 @@
+import datetime
+
+import pytest
+
+from trackintel.preprocessing.util import calc_temp_overlap
+
+
+@pytest.fixture()
+def time_1():
+    return datetime.datetime(year=1, month=1, day=1, hour=0, minute=0, second=0)
+
+
+@pytest.fixture()
+def one_hour():
+    return datetime.timedelta(hours=1)
+
+
+class TestCalc_temp_overlap():
+    def test_same_interval(self, time_1, one_hour):
+        ratio = calc_temp_overlap(time_1, time_1 + one_hour, time_1, time_1 + one_hour)
+        assert ratio == 1
+
+    def test_1_in_2(self, time_1, one_hour):
+        ratio = calc_temp_overlap(time_1, time_1 + one_hour, time_1, time_1 + 2 * one_hour)
+        assert ratio == 1
+
+    def test_2_in_1(self, time_1, one_hour):
+        ratio = calc_temp_overlap(time_1, time_1 + 2 * one_hour, time_1, time_1 + one_hour)
+        assert ratio == 0.5
+
+    def test_no_overlap(self, time_1, one_hour):
+        ratio = calc_temp_overlap(time_1, time_1 + one_hour, time_1 + one_hour, time_1 + 2 * one_hour)
+        assert ratio == 0
