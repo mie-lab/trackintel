4e16938603ec332274fc51e27095552b84b419c9
Henry Martin <31069329+henrymartin1@users.noreply.github.com>
2021-03-19 17:18:00 +0100

    CLN: Added missing docstring (#171)
    * DOC: Added docstring and doc for create_activity_flag
    
    * CLN: Minor adjustment of docstring
    
    * DOC: adjust doc structure
    
    Co-authored-by: Ye Hong <hongy_gz@foxmail.com>

--- trackintel/preprocessing/staypoints.py
+++ trackintel/preprocessing/staypoints.py
@@ -1,7 +1,6 @@
 import datetime
 from math import radians
 
-import geopandas as gpd
 import numpy as np
 from shapely.geometry import Point
 from sklearn.cluster import DBSCAN
@@ -173,22 +172,35 @@ def generate_locations(staypoints,
     return ret_stps, ret_loc
 
 
-def create_activity_flag(staypoints, method='time_threshold', time_threshold=5, activity_column_name='activity'):           
+def create_activity_flag(staypoints, method='time_threshold', time_threshold=5., activity_column_name='activity'):
     """
+    Add a flag whether or not a staypoint is considered an activity.
 
     Parameters
     ----------
-    staypoints
-    method
-    time_threshold
-    activity_column_name
+    staypoints: GeoDataFrame (as trackintel staypoints)
+        The original input staypoints
+
+    method: {'time_threshold'}, default = 'time_threshold'
+
+        - 'time_threshold' : All staypoints with a duration greater than the time_threshold are considered an activity.
+
+    time_threshold : float, default = 5 (minutes)
+        The time threshold for which a staypoint is considered an activity in minutes. Used by method 'time_threshold'
+    
+    activity_column_name : str , default = 'activity'
+        The name of the newly created column that holds the activity flag.
 
     Returns
     -------
+    staypoints : GeoDataFrame (as trackintel staypoints)
+        Original staypoints with the additional activity column
 
+    Examples
+    --------
+    >>> spts  = spts.as_staypoints.create_activity_flag(method='time_threshold', time_threshold=5)
+    >>> print(spts['activity'])
     """
-
-
     if method == 'time_threshold':
         staypoints[activity_column_name] = staypoints['finished_at'] - staypoints['started_at'] \
                                            > datetime.timedelta(minutes=time_threshold)
