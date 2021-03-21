57284771bc6ccfee7e32797b4b5eb04fddc3eb58
Ye Hong <hongy_gz@foxmail.com>
2021-03-21 17:16:53 +0100

    CLN: code clean up

--- tests/io/test_dataset_reader.py
+++ tests/io/test_dataset_reader.py
@@ -82,7 +82,6 @@ class TestReadGeolife:
         pfs, _ = read_geolife(os.path.join('tests', 'data', 'geolife'))
         tmp_file = os.path.join('tests', 'data', 'positionfixes_test.csv')
         pfs.as_positionfixes.to_csv(tmp_file)
-        print(pfs)
         pfs2 = ti.read_positionfixes_csv(tmp_file, index_col='id')[pfs.columns]
         os.remove(tmp_file)
         assert np.isclose(0, (pfs.lat - pfs2.lat).abs().sum())
