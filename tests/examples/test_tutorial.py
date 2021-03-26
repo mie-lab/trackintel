b83537bb4b51343c1c38e3e66b9b44da18b1057a
Sven Ruf <>
2021-03-26 10:56:41 +0100

    added correct change of working directory for tutorial test using pytest.fixture

--- tests/examples/test_tutorial.py
+++ tests/examples/test_tutorial.py
@@ -6,8 +6,14 @@ import trackintel as ti
 
 import pytest
 
+@pytest.fixture(scope="function")
+def change_test_dir():
+    os.chdir('./examples')
+    yield
+    os.chdir('..')
+
 class Test_tutorial():
-    def test_basic_tutorial(self):
+    def test_basic_tutorial(self, change_test_dir):
         """
         Tests if the tutorial jupyter notebook runs without errors
 
@@ -16,6 +22,7 @@ class Test_tutorial():
         None.
 
         """
-        
-        os.popen("jupyter nbconvert --to script --execute --stdout ./examples/trackintel_basic_tutorial.ipynb | python3").read()
-        os.remove('./examples/trackintel_basic_tutorial.py')
+  
+        os.popen("jupyter nbconvert --to script --execute --stdout ./trackintel_basic_tutorial.ipynb | python3").read()
+        #os.remove('./trackintel_basic_tutorial.py')
+
