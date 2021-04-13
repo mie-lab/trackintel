214f483d564fff6b534ae9076e1b5b4d1d6d947e
Christof Leutenegger <christof.leutenegger@hotmail.com>
2021-04-13 12:24:48 +0200

    TST: Added test for PostGIS export without any CRS.

--- tests/io/test_postgis_gpd.py
+++ tests/io/test_postgis_gpd.py
@@ -56,12 +56,14 @@ def conn_string_postgis():
     host = os.environ.get("PGHOST")
     port = os.environ.get("PGPORT")
 
-    conn_string = "postgresql://{}:{}@{}:{}/{}".format(user, password, host, port, dbname)
+    conn_string = "postgresql://{}:{}@{}:{}/{}".format(
+        user, password, host, port, dbname
+    )
     return conn_string
 
 
 @pytest.fixture()
-def connection_postgis():
+def conn_postgis():
     """
     Initiates a connection to a postGIS database that must already exist.
     See create_postgis for more information.
@@ -74,11 +76,20 @@ def connection_postgis():
     host = os.environ.get("PGHOST")
     port = os.environ.get("PGPORT")
     try:
-        con = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+        con = psycopg2.connect(
+            dbname=dbname, user=user, password=password, host=host, port=port
+        )
     except psycopg2.OperationalError:
         try:
             # psycopg2.connect gives operational error due to unsupported frontend protocol in conda environment
-            con = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port, sslmode="disable")
+            con = psycopg2.connect(
+                dbname=dbname,
+                user=user,
+                password=password,
+                host=host,
+                port=port,
+                sslmode="disable",
+            )
         except psycopg2.OperationalError:
             pytest.skip("Cannot connect with postgresql database")
 
@@ -132,9 +143,15 @@ def example_staypoints():
 @pytest.fixture
 def example_triplegs():
     # three linestring geometries that are only slightly different (last coordinate)
-    g1 = LineString([(13.476808430, 48.573711823), (11.5675446, 48.1485459), (8.5067847, 47.4)])
-    g2 = LineString([(13.476808430, 48.573711823), (11.5675446, 48.1485459), (8.5067847, 47.5)])
-    g3 = LineString([(13.476808430, 48.573711823), (11.5675446, 48.1485459), (8.5067847, 47.6)])
+    g1 = LineString(
+        [(13.476808430, 48.573711823), (11.5675446, 48.1485459), (8.5067847, 47.4)]
+    )
+    g2 = LineString(
+        [(13.476808430, 48.573711823), (11.5675446, 48.1485459), (8.5067847, 47.5)]
+    )
+    g3 = LineString(
+        [(13.476808430, 48.573711823), (11.5675446, 48.1485459), (8.5067847, 47.6)]
+    )
 
     t1 = pd.Timestamp("1971-01-01 00:00:00", tz="utc")
     t2 = pd.Timestamp("1971-01-01 05:00:00", tz="utc")
@@ -144,7 +161,13 @@ def example_triplegs():
     list_dict = [
         {"id": 0, "user_id": 0, "started_at": t1, "finished_at": t2, "geometry": g1},
         {"id": 1, "user_id": 0, "started_at": t2, "finished_at": t3, "geometry": g2},
-        {"id": 2, "user_id": 1, "started_at": t3, "finished_at": t3 + one_hour, "geometry": g3},
+        {
+            "id": 2,
+            "user_id": 1,
+            "started_at": t3,
+            "finished_at": t3 + one_hour,
+            "geometry": g3,
+        },
     ]
 
     tpls = gpd.GeoDataFrame(data=list_dict, geometry="geometry", crs="EPSG:4326")
@@ -160,7 +183,11 @@ def example_locations():
     p2 = Point(8.5067847, 47.5)
     p3 = Point(8.5067847, 47.6)
 
-    list_dict = [{"user_id": 0, "center": p1}, {"user_id": 0, "center": p2}, {"user_id": 1, "center": p3}]
+    list_dict = [
+        {"user_id": 0, "center": p1},
+        {"user_id": 0, "center": p2},
+        {"user_id": 1, "center": p3},
+    ]
     spts = gpd.GeoDataFrame(data=list_dict, geometry="center", crs="EPSG:4326")
     spts.index.name = "id"
     assert spts.as_locations
@@ -175,8 +202,20 @@ def example_trips():
     h = datetime.timedelta(hours=1)
 
     list_dict = [
-        {"user_id": 0, "started_at": t1, "finished_at": t2, "origin_staypoint_id": 0, "destination_staypoint_id": 1},
-        {"user_id": 0, "started_at": t2, "finished_at": t3, "origin_staypoint_id": 1, "destination_staypoint_id": 2},
+        {
+            "user_id": 0,
+            "started_at": t1,
+            "finished_at": t2,
+            "origin_staypoint_id": 0,
+            "destination_staypoint_id": 1,
+        },
+        {
+            "user_id": 0,
+            "started_at": t2,
+            "finished_at": t3,
+            "origin_staypoint_id": 1,
+            "destination_staypoint_id": 2,
+        },
         {
             "user_id": 1,
             "started_at": t3,
@@ -201,8 +240,10 @@ def del_table(con, table):
 
 
 class TestPositionfixes:
-    def test_read_write_positionfixes(self, example_positionfixes, conn_string_postgis, connection_postgis):
-        pfs = example_positionfixes
+    def test_io_positionfixes(
+        self, example_positionfixes, conn_string_postgis, conn_postgis
+    ):
+        pfs = example_positionfixes.copy()
         cs = conn_string_postgis
         table = "positionfixes"
         geom_col = pfs.geometry.name
@@ -213,13 +254,30 @@ class TestPositionfixes:
             pfs_db = pfs_db.set_index("id")
             assert_geodataframe_equal(pfs, pfs_db)
         finally:
-            del_table(connection_postgis, table)
+            del_table(conn_postgis, table)
+        pass
+
+    def test_no_crs_setting(
+        self, example_positionfixes, conn_string_postgis, conn_postgis
+    ):
+        pfs = example_positionfixes.copy()
+        cs = conn_string_postgis
+        table = "positionfixes"
+        geom_col = pfs.geometry.name
+        pfs.crs = None
+        try:
+            pfs.as_positionfixes.to_postgis(cs, table)
+            pfs_db = ti.io.read_positionfixes_postgis(cs, table, geom_col)
+            pfs_db = pfs_db.set_index("id")
+            assert_geodataframe_equal(pfs, pfs_db)
+        finally:
+            del_table(conn_postgis, table)
         pass
 
 
 class TestStaypoints:
-    def test_read_write_staypoints(self, example_staypoints, conn_string_postgis, connection_postgis):
-        spts = example_staypoints
+    def test_io_staypoints(self, example_staypoints, conn_string_postgis, conn_postgis):
+        spts = example_staypoints.copy()
         cs = conn_string_postgis
         table = "staypoints"
         geom_col = spts.geometry.name
@@ -229,12 +287,27 @@ class TestStaypoints:
             spts_db = ti.io.read_staypoints_postgis(cs, table, geom_col)
             assert_geodataframe_equal(spts, spts_db)
         finally:
-            del_table(connection_postgis, table)
+            del_table(conn_postgis, table)
+
+    def test_no_crs_setting(
+        self, example_staypoints, conn_string_postgis, conn_postgis
+    ):
+        spts = example_staypoints.copy()
+        cs = conn_string_postgis
+        table = "staypoints"
+        geom_col = example_staypoints.geometry.name
+        spts.crs = None
+        try:
+            spts.as_staypoints.to_postgis(cs, table)
+            spts_db = ti.io.read_staypoints_postgis(cs, table, geom_col)
+            assert_geodataframe_equal(spts, spts_db)
+        finally:
+            del_table(conn_postgis, table)
 
 
 class TestTriplegs:
-    def test_read_write_triplegs(self, example_triplegs, conn_string_postgis, connection_postgis):
-        tpls = example_triplegs
+    def test_io_triplegs(self, example_triplegs, conn_string_postgis, conn_postgis):
+        tpls = example_triplegs.copy()
         cs = conn_string_postgis
         table = "triplegs"
         geom_col = tpls.geometry.name
@@ -244,12 +317,26 @@ class TestTriplegs:
             tpls_db = ti.io.read_triplegs_postgis(cs, table, geom_col)
             assert_geodataframe_equal(tpls, tpls_db)
         finally:
-            del_table(connection_postgis, table)
+            del_table(conn_postgis, table)
+
+    def test_no_crs_setting(self, example_triplegs, conn_string_postgis, conn_postgis):
+        tpls = example_triplegs.copy()
+        cs = conn_string_postgis
+        table = "triplegs"
+        geom_col = tpls.geometry.name
+        tpls.crs = None
+
+        try:
+            tpls.as_triplegs.to_postgis(cs, table)
+            tpls_db = ti.io.read_triplegs_postgis(cs, table, geom_col)
+            assert_geodataframe_equal(tpls, tpls_db)
+        finally:
+            del_table(conn_postgis, table)
 
 
 class TestLocations:
-    def test_read_write_locations(self, example_locations, conn_string_postgis, connection_postgis):
-        locs = example_locations
+    def test_io_locations(self, example_locations, conn_string_postgis, conn_postgis):
+        locs = example_locations.copy()
         cs = conn_string_postgis
         table = "locations"
         geom_col = locs.geometry.name
@@ -259,12 +346,25 @@ class TestLocations:
             locs_db = ti.io.read_locations_postgis(cs, table, geom_col)
             assert_geodataframe_equal(locs, locs_db)
         finally:
-            del_table(connection_postgis, table)
+            del_table(conn_postgis, table)
+
+    def test_no_crs_setting(self, example_locations, conn_string_postgis, conn_postgis):
+        locs = example_locations.copy()
+        cs = conn_string_postgis
+        table = "locations"
+        geom_col = locs.geometry.name
+        locs.crs = None
+        try:
+            locs.as_locations.to_postgis(cs, table)
+            locs_db = ti.io.read_locations_postgis(cs, table, geom_col)
+            assert_geodataframe_equal(locs, locs_db)
+        finally:
+            del_table(conn_postgis, table)
 
 
 class TestTrips:
-    def test_read_write_trips(self, example_trips, conn_string_postgis, connection_postgis):
-        trips = example_trips
+    def test_io_trips(self, example_trips, conn_string_postgis, conn_postgis):
+        trips = example_trips.copy()
         cs = conn_string_postgis
         table = "trips"
 
@@ -273,4 +373,4 @@ class TestTrips:
             trips_db = ti.io.read_trips_postgis(cs, table)
             assert_frame_equal(trips, trips_db)
         finally:
-            del_table(connection_postgis, table)
+            del_table(conn_postgis, table)
