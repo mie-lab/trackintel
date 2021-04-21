0e189b4abd56104205b9419359d3072053aff5c1
Christof Leutenegger <christof.leutenegger@posteo.ch>
2021-04-21 03:11:29 +0200

    ENH: Skelleton for location identifier.

---
+++ trackintel/analysis/location_identification.py
@@ -0,0 +1,41 @@
+def location_identifier(sps, pre_filter=True, recipe="FREQ"):
+    """
+
+    Parameters
+    ----------
+    sps
+    pre_filter
+    recipe
+
+    Returns
+    -------
+
+    """
+    pass
+
+
+def pre_filter(distinct_locs, staypoints_at_loc, distinct_hours_at_loc, num_of_day_active_at_loc):
+    """
+    something
+    Parameters
+    ----------
+    distinct_locs
+    staypoints_at_loc
+    distinct_hours_at_loc
+    num_of_day_active_at_loc
+
+    Returns
+    -------
+
+    """
+    pass
+
+
+def freq_recipe():
+    """
+
+    Returns
+    -------
+
+    """
+    pass
