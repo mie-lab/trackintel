b830ddab5f03af318fa02b20e045d5d2691952ff
Christof Leutenegger <christof.leutenegger@hotmail.com>
2021-04-27 15:05:52 +0200

    DOC: Improved documentation.

--- trackintel/analysis/location_identification.py
+++ trackintel/analysis/location_identification.py
@@ -1,3 +1,8 @@
+_RECEIPTS = {
+    "FREQ": freq_receipt,
+}
+
+
 def location_identifier(sps, pre_filter=True, receipt="FREQ"):
     """Finde home and work/location for each user with different receipts.
 
@@ -35,10 +40,23 @@ def location_identifier(sps, pre_filter=True, receipt="FREQ"):
     --------
     >>> ti.analysis.location_identification.location_idenifier(sps, pre_filter=True, receipt="FREQ")
     """
-    pass
+    # what do we do here?
+    # we take the gdf and assert two things 1. is staypoint 2. has location_id column
+    assert sps.as_staypoints
+    if "location_id" not in sps.columns:
+        raise KeyError((
+            "To derive location activities the GeoDataFrame (as trackintel staypoints)must have a column "
+            f"named 'location_id' but it has [{', '.join(sps.columns)}]"))
+    # then hand it to to the filter function if necessary.
+    if pre_filter:
+        sps = pre_filter_locations()
+
+    m = _RECEIPTS[receipt]()  # das müssen wir mal schöner machen.
+
+    return m
 
 
-def pre_filter(sps, tresh_sp, thresh_min_locs, tresh_sp_at_locs, thresh_time, thresh_period):
+def pre_filter_locations(sps, tresh_sp, thresh_min_locs, tresh_sp_at_locs, thresh_time, thresh_period):
     """Filter locations and user out that have not enough data to do a proper analysis.
 
     Parameters
@@ -58,17 +76,22 @@ def pre_filter(sps, tresh_sp, thresh_min_locs, tresh_sp_at_locs, thresh_time, th
 
     Returns
     -------
-    np.array
+    GeoDataFrame (as trackintel staypoints)
         A boolean index arrray with True everywhere the Bedingungen are erfüllt.
 
     Examples
     --------
     >> do something
     """
-    pass
+    return sps.copy()
 
 
-def freq_recipe():
+def freq_receipt(sps):
     """Docstring here :D
     """
+
     pass
+
+
+def staypoints_with_location_assertion(funct):
+    
