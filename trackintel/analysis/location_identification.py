cc0b4e342688863502f71f85709651a6731ffa39
Christof Leutenegger <christof.leutenegger@hotmail.com>
2021-05-19 18:06:12 +0200

    CLN: review suggestions.

--- trackintel/analysis/location_identification.py
+++ trackintel/analysis/location_identification.py
@@ -3,16 +3,16 @@ import pandas as pd
 import trackintel as ti
 
 
-def location_identifier(sps, recipe="FREQ", pre_filter=True, **pre_filter_kwargs):
-    """Assign "home" and "work" activity label for each user with different recipes.
+def location_identifier(spts, method="FREQ", pre_filter=True, **pre_filter_kwargs):
+    """Assign "home" and "work" activity label for each user with different methods.
 
     Parameters
     ----------
-    sps : Geodataframe (as trackintel staypoints)
+    spts : Geodataframe (as trackintel staypoints)
         Staypoints with column "location_id".
 
-    recipe : {'FREQ'}, default "FREQ"
-        Choose which recipe to use.
+    method : {'FREQ'}, default "FREQ"
+        Choose which method to use.
 
         - FREQ: "Generate a activity label per user by assigning the most visited location the label "home"
           and the second most visited location the label "work". The remaining locations get no label.
@@ -31,7 +31,7 @@ def location_identifier(sps, recipe="FREQ", pre_filter=True, **pre_filter_kwargs
 
     Note
     ----
-    The recipe are adapted from [1]. The original algorithms count the distinct hours at a
+    The methods are adapted from [1]. The original algorithms count the distinct hours at a
     location as the home location is derived from geo-tagged tweets. We directly sum the time
     spent at a location as our data model includes that.
 
@@ -44,33 +44,33 @@ def location_identifier(sps, recipe="FREQ", pre_filter=True, **pre_filter_kwargs
 
     Examples
     --------
-    >>> ti.analysis.location_identifier(sps, pre_filter=True, recipe="FREQ")
+    >>> ti.analysis.location_identifier(spts, pre_filter=True, method="FREQ")
     """
-    assert sps.as_staypoints
-    sps = sps.copy()
-    if "location_id" not in sps.columns:
+    assert spts.as_staypoints
+    spts = spts.copy()
+    if "location_id" not in spts.columns:
         raise KeyError(
             (
                 "To derive activity labels the GeoDataFrame (as trackintel staypoints) must have a column "
-                f"named 'location_id' but it has [{', '.join(sps.columns)}]"
+                f"named 'location_id' but it has [{', '.join(spts.columns)}]"
             )
         )
     if pre_filter:
-        f = pre_filter_locations(sps, **pre_filter_kwargs)
+        f = pre_filter_locations(spts, **pre_filter_kwargs)
     else:
-        f = pd.Series(np.full(len(sps.index), True))
+        f = pd.Series(np.full(len(spts.index), True))
 
-    if recipe == "FREQ":
-        recipe_val = freq_recipe(sps[f], "home", "work")
+    if method == "FREQ":
+        method_val = freq_method(spts[f], "home", "work")
     else:
-        raise ValueError(f"Recipe {recipe} does not exist.")
+        raise ValueError(f"Method {method} does not exist.")
 
-    sps.loc[f, "activity_label"] = recipe_val["activity_label"]
-    return sps
+    spts.loc[f, "activity_label"] = method_val["activity_label"]
+    return spts
 
 
 def pre_filter_locations(
-    sps,
+    spts,
     agg_level="user",
     thresh_sp=10,
     thresh_loc=10,
@@ -84,7 +84,7 @@ def pre_filter_locations(
 
     Parameters
     ----------
-    sps : GeoDataFrame (as trackintel staypoints)
+    spts : GeoDataFrame (as trackintel staypoints)
         Staypoints with the column "location_id".
 
     agg_level: {"user", "dataset"}, default "user"
@@ -116,33 +116,33 @@ def pre_filter_locations(
 
     Examples
     --------
-    >> mask = ti.analysis.pre_filter_locations(sps)
-    >> sps = sps[mask]
+    >> mask = ti.analysis.pre_filter_locations(spts)
+    >> spts = spts[mask]
     """
-    assert sps.as_staypoints
-    sps = sps.copy()
+    assert spts.as_staypoints
+    spts = spts.copy()
     if isinstance(thresh_loc_time, str):
         thresh_loc_time = pd.to_timedelta(thresh_loc_time)
     if isinstance(thresh_loc_period, str):
         thresh_loc_period = pd.to_timedelta(thresh_loc_period)
 
     # filtering users
-    user = sps.groupby("user_id").nunique()
+    user = spts.groupby("user_id").nunique()
     user_sp = user["started_at"] >= thresh_sp  # every staypoint should have a started_at -> count
     user_loc = user["location_id"] >= thresh_loc
     user_filter_agg = user_sp & user_loc
     user_filter_agg.rename("user_filter", inplace=True)  # rename for merging
-    user_filter = pd.merge(sps["user_id"], user_filter_agg, left_on="user_id", right_index=True)["user_filter"]
+    user_filter = pd.merge(spts["user_id"], user_filter_agg, left_on="user_id", right_index=True)["user_filter"]
 
     # filtering locations
-    sps["duration"] = sps["finished_at"] - sps["started_at"]
+    spts["duration"] = spts["finished_at"] - spts["started_at"]
     if agg_level == "user":
         groupby_loc = ["user_id", "location_id"]
     elif agg_level == "dataset":
         groupby_loc = ["location_id"]
     else:
         raise ValueError(f"Unknown agg_level '{agg_level}' use instead {{'user', 'dataset'}}.")
-    loc = sps.groupby(groupby_loc).agg({"started_at": [min, "count"], "finished_at": max, "duration": sum})
+    loc = spts.groupby(groupby_loc).agg({"started_at": [min, "count"], "finished_at": max, "duration": sum})
     loc.columns = loc.columns.droplevel(0)  # remove possible multi-index
     loc.rename(columns={"min": "started_at", "max": "finished_at", "sum": "duration"}, inplace=True)
     # period for maximal time span first visit - last visit.
@@ -153,21 +153,21 @@ def pre_filter_locations(
     loc_period = loc["period"] >= thresh_loc_period
     loc_filter_agg = loc_sp & loc_time & loc_period
     loc_filter_agg.rename("loc_filter", inplace=True)  # rename for merging
-    loc_filter = pd.merge(sps[groupby_loc], loc_filter_agg.reset_index(), on=groupby_loc, how="left")["loc_filter"]
+    loc_filter = pd.merge(spts[groupby_loc], loc_filter_agg.reset_index(), on=groupby_loc, how="left")["loc_filter"]
 
     total_filter = user_filter & loc_filter
 
     return total_filter
 
 
-def freq_recipe(sps, *labels):
+def freq_method(spts, *labels):
     """Generate an activity label per user by assigning the most visited location the label "home"
     and the second most visited location the label "work" or assign your own labels. The remaining
     locations get no label.
 
     Parameters
     ----------
-    sps : GeoDataFrame (as trackintel staypoints)
+    spts : GeoDataFrame (as trackintel staypoints)
         Staypoints with the column "location_id".
     labels : collection of str, default ("home", "work")
         Labels in decreasing time of activity.
@@ -179,21 +179,21 @@ def freq_recipe(sps, *labels):
 
     Examples
     --------
-    >> staypoints = ti.analysis.freq_recipe(staypoints, "home", "work")
+    >> staypoints = ti.analysis.freq_method(staypoints, "home", "work")
     """
-    sps = sps.copy()
+    spts = spts.copy()
     if not labels:
         labels = ("home", "work")
-    for name, group in sps.groupby("user_id"):
+    for name, group in spts.groupby("user_id"):
         if "duration" not in group.columns:
             group["duration"] = group["finished_at"] - group["started_at"]
         # pandas keeps inner order of groups
-        sps.loc[sps["user_id"] == name, "activity_label"] = _freq_transform(group, *labels)
-    return sps
+        spts.loc[spts["user_id"] == name, "activity_label"] = _freq_transform(group, *labels)
+    return spts
 
 
 def _freq_transform(group, *labels):
-    """Transform function that assigns the longest visited data point the labels in order.
+    """Transform function that assigns the longest (duration) visited locations the labels in order.
 
     Parameters
     ----------
