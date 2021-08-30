c7af0a4043ddfd92ed4780dbeb250b2be9e17a6b
Ye <hongy_gz@foxmail.com>
2021-08-30 15:33:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ else:
 # Where the magic happens:
 setup(
     name=NAME,
-    version=__version__,
+    version=about["__version__"],
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type="text/markdown",
