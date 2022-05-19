6f4eab6ed50bf96dd400523d37b38135a4f77e6a
Christof <christof.leutenegger@hotmail.com>
2022-05-20 00:52:59 +0200

    ENH: increase coverage for calc_temp_overlap

--- trackintel/preprocessing/util.py
+++ trackintel/preprocessing/util.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 import pandas as pd
 from joblib import Parallel, delayed
 from tqdm import tqdm
@@ -28,46 +29,14 @@ def calc_temp_overlap(start_1, end_1, start_2, end_2):
     >>> ti.preprocessing.calc_temp_overlap(start_1, end_1, start_2, end_2)
 
     """
-
-    # case 1: no overlap - 1 was before 2
-    if end_1 < start_2:
-        return 0
-
-    # case 2: no overlap - 1 comes after 2
-    elif end_2 < start_1:
-        return 0
-
-    # case 3: 2 fully in 1
-    if (start_1 <= start_2) and (end_1 >= end_2):
-        temp_overlap = end_2 - start_2
-
-    # case 4: 1 fully in 2
-    elif (start_2 <= start_1) and (end_2 >= end_1):
-        temp_overlap = end_1 - start_1
-
-    # case 5: 1 overlaps 2 from right
-    elif (start_2 <= start_1) and (end_2 <= end_1):
-        temp_overlap = end_2 - start_1
-
-    # case 6: 1 overlaps 2 from left
-    elif (start_1 <= start_2) and (end_1 <= end_2):
-        temp_overlap = end_1 - start_2
-
-    else:
-        raise Exception("wrong case")
-
-    temp_overlap = temp_overlap.total_seconds()
-
-    # no overlap at all
-    assert temp_overlap >= 0, "the overlap can not be lower than 0"
+    start = max(start_1, start_2)
+    end = min(end_1, end_2)
+    temp_overlap = max(timedelta(0), end-start)
 
     dur = end_1 - start_1
-    if dur.total_seconds() == 0:
-        return 0
-    else:
-        overlap_ratio = temp_overlap / dur.total_seconds()
-
-    return overlap_ratio
+    if dur <= timedelta(0):
+        return 0  # either invalid or division 0
+    return temp_overlap / dur
 
 
 def applyParallel(dfGrouped, func, n_jobs, print_progress, **kwargs):
