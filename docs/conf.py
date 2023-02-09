f942def1d21665dc0ffa671ecb9dd86adccfa981
Ye <hongy_gz@foxmail.com>
2023-02-09 17:07:55 +0100

    add ci

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ autodoc_mock_imports = [
 # -- Project information -----------------------------------------------------
 
 project = "trackintel"
-copyright = "2019 - 2022, Dominik Bucher, Henry Martin, Ye Hong, Nina Wiedemann"
+copyright = "2019 - 2023, Dominik Bucher, Henry Martin, Ye Hong, Nina Wiedemann"
 author = "Dominik Bucher, Henry Martin, Ye Hong, Nina Wiedemann"
 
 
