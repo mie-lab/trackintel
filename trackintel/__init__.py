a0dbb6589c023c75e5c9b4eedaa30693c778d1ed
Christof <christof.leutenegger@hotmail.com>
2023-09-21 12:12:49 +0200

    ENH: clean up __init__ files

--- trackintel/__init__.py
+++ trackintel/__init__.py
@@ -1,15 +1,3 @@
-import trackintel.model
-import trackintel.analysis
-import trackintel.geogr
-import trackintel.preprocessing
-
-from trackintel.io.file import read_positionfixes_csv
-from trackintel.io.file import read_triplegs_csv
-from trackintel.io.file import read_staypoints_csv
-from trackintel.io.file import read_locations_csv
-from trackintel.io.file import read_trips_csv
-from trackintel.io.file import read_tours_csv
-
 from trackintel.model.positionfixes import Positionfixes
 from trackintel.model.locations import Locations
 from trackintel.model.triplegs import Triplegs
@@ -19,7 +7,34 @@ from trackintel.model.trips import TripsDataFrame
 from trackintel.model.trips import TripsGeoDataFrame
 from trackintel.model.tours import Tours
 
+from trackintel.io.file import read_positionfixes_csv
+from trackintel.io.file import read_triplegs_csv
+from trackintel.io.file import read_staypoints_csv
+from trackintel.io.file import read_locations_csv
+from trackintel.io.file import read_trips_csv
+from trackintel.io.file import read_tours_csv
+
 from trackintel.visualization import plot, plot_modal_split
 
 from trackintel.__version__ import __version__
 from .core import print_version
+
+__all__ = [
+    "Positionfixes",
+    "Locations",
+    "Triplegs",
+    "Staypoints",
+    "Trips",
+    "TripsDataFrame",
+    "TripsGeoDataFrame",
+    "Tours",
+    "read_positionfixes_csv",
+    "read_triplegs_csv",
+    "read_staypoints_csv",
+    "read_locations_csv",
+    "read_trips_csv",
+    "read_tours_csv",
+    "plot",
+    "plot_modal_split",
+    "print_version",
+]
