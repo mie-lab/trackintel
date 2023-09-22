93ad546373e318607e6b364c295393c95b7747ef
Christof Leutenegger <49416778+bifbof@users.noreply.github.com>
2023-09-22 15:50:27 +0200

    ENH: clean up __init__ files (#536)

--- trackintel/model/__init__.py
+++
@@ -1,6 +0,0 @@
-from .positionfixes import Positionfixes
-from .locations import Locations
-from .triplegs import Triplegs
-from .staypoints import Staypoints
-from .trips import Trips
-from .tours import Tours
