68c142c86155f67da2b2bf3208839ab349dbc41a
Ye Hong <hongy_gz@foxmail.com>
2024-12-06 16:28:09 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -248,7 +248,7 @@ epub_exclude_files = ["search.html"]
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {"https://docs.python.org/": None}
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 
 
 # Prevent that all member and method tables are tables of contents (numpydoc).
